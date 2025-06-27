bd1468f72fa9c9975eec64c2c9f2306ee50b649f
maherkefi123 <maher1.kefi@gmail.com>
2025-06-27 20:55:56 +0000

    test juint

---
+++ src/test/java/tn/esprit/service/SimpleTest.java
@@ -0,0 +1,13 @@
+package tn.esprit.service;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SimpleTest {
+
+    @Test
+    void addition() {
+        int result = 2 + 2;
+        assertEquals(4, result);
+    }
+}
